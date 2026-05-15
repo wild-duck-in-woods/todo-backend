eb8ac4fb366084f0696b989c06dc2f2a7e4dc031
wild-duck-in-woods <ajaykalindala@gmail.com>
2026-05-15 12:12:01 +0530

    added vercel link to cors

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const PORT = process.env.PORT || 5000
 
 //app.use(cors())
 app.use(cors({
-    origin: [
-        "http://localhost:5173"
-    ]
-}))
+   origin: "https://react-to-do-app-dusky.vercel.app"
+}));
 app.use(express.json())
 app.use(authRoutes)
 app.use(taskRoutes)
